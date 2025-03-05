e726b52c37769792af8431b988aaa5c0c9ad615e
BogdanSEST2 <tsybin_b@auca.kg>
2025-03-05 13:55:56 +0600

    Сделал карточку на реакте и стилизовал её с использованием tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Card from './components/card';
+
+
+
+
+function App() {
+  return (
+    <div className='w-[100%] h-[100vh] flex justify-center items-center p-3 gap-4'>
+      <Card />
+    </div>
+  )
+}
+
+export default App
